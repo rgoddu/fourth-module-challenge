5820f19e0d5ee675f5c351cbdd2c81a5f6f41f76
Remi G <rgoddu909@gmail.com>
2023-10-08 21:42:31 -0400

    starter code

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// init()
+    //creates object with quiz answers
+    // creates object with quiz prompts and options
+    //create start button
+    //when start button is pressed, starts timer and queues first question
+
+function startQuiz(){
+//creates question prompt
+    //creates four option buttons
+    
+    //creates next button
+    //removes start button element
+}
+
+function questionOne(){
+    //queues the first question
+    //on click event, 
+}
+function questionTwo(){
+
+}
