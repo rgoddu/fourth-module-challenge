cd34f17cb2eed8fbda6a3fe83b7769decd4c36c6
Remi G <rgoddu909@gmail.com>
2023-10-08 22:36:05 -0400

    pseudocode

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,22 @@
 //creates object with logged answers
 //create start button
 
+function startQuiz() {
+    //when start button is pressed:
+
+    //removes start button element
+    //creates question prompt
+    //creates four option buttons
+    //creates next button
+    // starts timer, queues first question, adds one to counter
+
+    //creates function - if button option is selected, saves counter and button choice to local storage
+}
+
 function nextQuestion() {
     // on selection of next button:
+    //saves student answer object to local storage
+
     //if counter is less than the length of the quiz
     //adds 1 to counter variable
     //pulls prompt and options from associated objects
@@ -16,6 +30,8 @@ function nextQuestion() {
 }
 function previousQuestion() {
     //on selection of the previous button:
+    //saves student answer object to local storage
+
     //if counter is >1:
     //removes 1 from counter variable
     //pulls prompt and options from associated objects
@@ -23,17 +39,6 @@ function previousQuestion() {
     //else if the counter is 1:
     //calls firstQuestion function
 
-}
-
-
-function startQuiz() {
-    //when start button is pressed:
-
-    //removes start button element
-    //creates question prompt
-    //creates four option buttons
-    //creates next button
-    // starts timer, queues first question, adds one to counter
 }
 
 function firstQuestion() {
@@ -48,4 +53,20 @@ function submitQuiz(){
     //removes all card elements
     //stops timer
     //loops to compares student answer object with correct answer object
+    //returns score
 }
+
+//questions:
+which dinosaur is best?
+triceratops
+t-Rex
+pterodactyl
+brontosaurus
+
+which dinosaur food is the most yummy?
+water
+leaves
+tree bark
+other dinosaurs
+
+
