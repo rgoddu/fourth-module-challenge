3fc2d0aeb264eb83fade23d871d1d6e27fada6ff
Remi G <rgoddu909@gmail.com>
2023-10-08 22:19:20 -0400

    psuedocode

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,44 @@
 // init()
-    //creates object with quiz answers
-    // creates object with quiz prompts and options
-    //create start button
-    //when start button is pressed, starts timer and queues first question
+//creates counter variable to store the question number
+//creates object with quiz answers
+//creates object with quiz prompts and options
+//creates object with logged answers
+//create start button
 
-function startQuiz(){
-//creates question prompt
-    //creates four option buttons
-    
-    //creates next button
-    //removes start button element
+function nextQuestion() {
+    // on selection of next button:
+    //if counter is less than the length of the quiz
+    //adds 1 to counter variable
+    //pulls prompt and options from associated objects
+
+    //else if counter is equal to the length of quiz:
+    //queues lastQuestion function
+}
+function previousQuestion() {
+    //on selection of the previous button:
+    //if counter is >1:
+    //removes 1 from counter variable
+    //pulls prompt and options from associated objects
+
+    //else if the counter is 1:
+    // calls firstQuestion function
+
 }
 
-function questionOne(){
+
+function startQuiz() {
+    //when start button is pressed:
+    //creates question prompt
+    //creates four option buttons
+    //creates next button
+    //removes start button element
+    // starts timer, queues first question, adds one to counter
+}
+
+function questionOne() {
     //queues the first question
     //on click event, 
 }
-function questionTwo(){
+function questionTwo() {
 
 }
