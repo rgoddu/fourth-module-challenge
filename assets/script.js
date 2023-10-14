21c1bc70810b96458cc923d7071da6e70d79fc34
christian goddu <rgoddu909@gmail.com>
2023-10-13 20:08:56 -0400

    nice progress!

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,10 @@
-// init()
-//creates counter variable to store the question number
+
 var questionNumber = 1
+var quizLength = 3
 var quizCard = document.getElementById("quiz-container")
-//creates object with quiz answers
-var quizLength = 5
+var navigatorArea = document.getElementById("navigator")
 class quizTemplate {
     constructor() {
-
     }
 }
 var answerKey = {
@@ -14,7 +12,6 @@ var answerKey = {
     question2Answer: "option1",
     question3Answer: "option3"
 }
-
 var quizPrompts = {
     question1: "Which dinosaur is best?",
     question2: "Which dinosaur food is the most yummiest?",
@@ -36,6 +33,9 @@ var quizOptions = {
     q3o2: "stegosaurus",
     q3o3: "allosaurus",
     q3o4: "ichthyornis"
+}
+var studentAnswers = {
+
 }
 
 //creates object with student answers
@@ -64,74 +64,98 @@ function createNextButton() {
     var nextButton = document.createElement("button")
     nextButton.setAttribute("id", "next-button")
     nextButton.textContent = "NEXT"
-    quizCard.append(nextButton)
+    navigatorArea.append(nextButton)
+    nextButton.addEventListener("click", nextQuestion)
+}
+function createPreviousButton() {
+    var previousButton = document.createElement("button")
+    previousButton.setAttribute("id", "previous-button")
+    previousButton.textContent = "PREV"
+    navigatorArea.prepend(previousButton)
+    previousButton.addEventListener("click", previousQuestion)
+}
+function loadQuestion() {
+    var promptSpace = document.getElementById("questionPrompt")
+    promptSpace.textContent = quizPrompts["question" + questionNumber]
+    for (var i = 1; i <= 4; i++) {
+        var optionSpace = document.getElementById("option" + i)
+        optionSpace.textContent = quizOptions["q" + questionNumber + "o" + i]
+    }
 }
 function startQuiz() {
     startQuizButton.remove()
     createQuestionPrompt()
     createOptionButtons()
     createNextButton()
-
-    questionNumber++
-    console.log(questionNumber)
-    nextQuestion()
+    loadQuestion()
+    console.log("hello, passed start")
 }
+selectAnswer()
+function selectAnswer() {
+    quizCard.addEventListener("click", (event) => {
+        var buttonClicked = event.target
+        var selectOption1 = document.getElementById("option1")
+        var selectOption2 = document.getElementById("option2")
+        var selectOption3 = document.getElementById("option3")
+        var selectOption4 = document.getElementById("option4")
+        if (buttonClicked === selectOption1) {
+            studentAnswers[questionNumber] = "option1"
 
-function loadPrompt(){
-    promptSpace = document.getElementById("questionPrompt")
-    promptSpace.textContent = quizPrompts["question"+questionNumber]
-}
-function loadOptions(){
-    
-    for(var i = 1; i<=4; i++){
-        optionSpace = document.getElementById("option" + i)
-        optionSpace.textContent = quizOptions["q"+questionNumber+"o"+i]
-    }
-}
-
-function nextQuestion() {
- 
-    //saves student answer object to local storage
-    var nextQButton = document.getElementById("next-button")
-    nextQButton.addEventListener("click", function () {
-        if (questionNumber = 1) {
-            //pulls prompt and options from associated objects
-loadPrompt()
-loadOptions()
-            // option1 = document.getElementById("option1")
-            // console.log(option1)
-            // option1.textContent = "hello"
-            questionNumber++
-            console.log(questionNumber)
-        } else if (1 < questionNumber && questionNumber < quizLength) {
-
-        } else {
-            // next question button is turned into submit button
+        } else if (buttonClicked === selectOption2) {
+            studentAnswers[questionNumber] = "option2"
         }
+        else if (buttonClicked === selectOption3) {
+            studentAnswers[questionNumber] = "option3"
+        }
+        else if (buttonClicked === selectOption4) {
+            studentAnswers[questionNumber] = "option4"
+        }
+        console.log(studentAnswers)
+    })
+}
+function nextQuestion() {
+    console.log("trip#2")
+    //saves student answer object to local storage
+
+    if (questionNumber === 1) {
+        questionNumber++
+        loadQuestion()
+        createPreviousButton()
+
+        console.log("triphere aosidf")
+    } else if ((1 < questionNumber) && (questionNumber < (quizLength-1))) {
+        questionNumber++
+        loadQuestion()
+
+    } else if (questionNumber === (quizLength-1)) {
+        questionNumber++
+        loadQuestion()
+        var nextQButton = document.getElementById("next-button")
+        nextQButton.textContent = "SUBMIT"
+        // next question button is turned into submit button
     }
-
-
-
-
-    )
 }
 function previousQuestion() {
     //on selection of the previous button:
-    //saves student answer object to local storage
+    //saves student answer object to local storage+
+    
+    if (questionNumber === 2) {
+        questionNumber-- 
+        loadQuestion()
+        prevQButton = document.getElementById("previous-button")
+        prevQButton.remove()
+    }
 
     //if counter is >1:
     //removes 1 from counter variable
     //pulls prompt and options from associated objects
 
     //else if the counter is 1:
-    //calls firstQuestion function
+
 
 }
 
-function firstQuestion() {
 
-    // previous question button is removed
-}
 
 function submitQuiz() {
     //removes all card elements
